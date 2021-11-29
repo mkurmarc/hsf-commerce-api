f414cdcc2a9a00acf1845cfd2f5bfff1645c91c9
mkurmarc <51379562+mkurmarc@users.noreply.github.com>
2021-11-28 23:15:57 -0800

    added db models

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
@@ -12,10 +13,15 @@ mongoose.connect(process.env.MONGO_URL)
     .catch((e) => {
         console.log(e)
     });
-
-//
+/*
+app.get("/api/test", () => {
+    console.log("test is successful");
+});
+*/
+app.use(express.json());
 app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(process.env.PORT || 5000, () => {
-    console.log("Backend server is running!")
+    console.log("Backend server is running!");
 });
--- routes/auth.js
+++ routes/auth.js
@@ -1,21 +1,28 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const CryptoJS = require("crypto-js");
 
 // register
+// TODO: if no credentials entered, respond with error 500
 router.post("/register", async (req, res) => {
+    // add other error responses, like status 400
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password
+        password: CryptoJS.AES.encrypt(
+            req.body.password, 
+            process.env.PASS_SEC).toString(),
     });
 
     try{
         const savedUser = await newUser.save();
-        console.log(savedUser);
+        res.status(201).json(savedUser);
     } catch(err) {
-        console.log(err);
+        res.status(500).json(err);
     }
-
 });
+
+// LOGIN
+
 
 module.exports = router
