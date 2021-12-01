c55149b720bc0282054fb479952708a1859a9a44
mkurmarc <51379562+mkurmarc@users.noreply.github.com>
2021-11-30 18:53:36 -0800

    created new routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 // register
 // TODO: if no credentials entered, respond with error 500
@@ -10,19 +11,52 @@ router.post("/register", async (req, res) => {
         username: req.body.username,
         email: req.body.email,
         password: CryptoJS.AES.encrypt(
-            req.body.password, 
-            process.env.PASS_SEC).toString(),
+            req.body.password,
+            process.env.PASS_SEC
+        ).toString(),
     });
-
-    try{
+    // block below actually saves to the DB
+    try {
         const savedUser = await newUser.save();
         res.status(201).json(savedUser);
-    } catch(err) {
+    } catch (err) {
         res.status(500).json(err);
     }
 });
 
 // LOGIN
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({ username: req.body.username });
+        !user && res.status(401).json("Wrong credentials!");
 
+        const hashedPassword = CryptoJS.AES.decrypt(
+            user.password,
+            process.env.PASS_SEC
+        );
+
+        const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+        OriginalPassword !== req.body.password &&
+            res.status(401).json("Wrong credentials!");
+
+        const accessToken = jwt.sign(
+            {
+                id: user._id,
+                isAdmin: user.isAdmin,
+            },
+            process.env.JWT_SEC,
+            { expiresIn: "3d" }
+        );
+
+
+        const { password, ...others } = user._doc;
+
+        res.status(200).json({ ...others, accessToken });
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router
